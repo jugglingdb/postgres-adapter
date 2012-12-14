bc96363f013e54427000587847f5754be8613fd5
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.com>
2012-12-14 17:41:39 +0400

    Update database credentials for travis

--- test/postgres.js
+++ test/postgres.js
@@ -2,7 +2,8 @@ var jdb = require('jugglingdb'),
     Schema = jdb.Schema,
     test = jdb.test,
     schema = new Schema(__dirname + '/..', {
-        database: ':memory:'
+        database: 'myapp_test',
+        username: 'postgres'
     });
 
 test(module.exports, schema);
