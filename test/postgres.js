f2e9fe32cbcc6bd8428994c5abb334eb53c4f955
Erin Noe-Payne <erinnp@mitre.org>
2013-01-10 14:45:04 -0500

    added basic tests

--- test/postgres.js
+++ test/postgres.js
@@ -2,9 +2,37 @@ var jdb = require('jugglingdb'),
     Schema = jdb.Schema,
     test = jdb.test,
     schema = new Schema(__dirname + '/..', {
-        database: 'myapp_test',
-        username: 'postgres'
+        database:'myapp_test',
+        username:'postgres'
     });
 
 test(module.exports, schema);
 
+
+test.it('all should support regex', function (test) {
+    Post = schema.models.Post;
+
+    Post.destroyAll(function () {
+        Post.create({title:'Postgres Test Title'}, function (err, post) {
+            var id = post.id
+            Post.all({where:{title:/^Postgres/}}, function (err, post) {
+                test.ok(!err);
+                test.ok(post[0].id == id);
+                test.done();
+            });
+        });
+    });
+});
+
+test.it('all should support arbitrary expressions', function (test) {
+    Post.destroyAll(function () {
+        Post.create({title:'Postgres Test Title'}, function (err, post) {
+            var id = post.id
+            Post.all({where:{title:{ilike:'postgres%'}}}, function (err, post) {
+                test.ok(!err);
+                test.ok(post[0].id == id);
+                test.done();
+            });
+        });
+    });
+})
